77c9c60c1b557a3ac62f65834e9c3006f5150874
MusaKamau <141785937+MusaKamau@users.noreply.github.com>
2023-09-13 00:18:35 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-var express = require('express');
-var cors = require('cors');
+const express = require('express');
+const cors = require('cors');
 require('dotenv').config()
 
-var app = express();
+const app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -11,8 +11,20 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+const multer = require('multer')
+const upload = multer({dest : './uploads/'})
 
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) =>{
 
+  try {
+    console.log(req.file)
+    const {originalname, mimetype, size} = req.file
+    res.json({"name": originalname, "type": mimetype, "size": size})
+  } catch (error) {
+    res.status(500).json({"success": false, "msg": "file upload failed"})
+  }
+  
+})
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
